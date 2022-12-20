84168dbe891e598e7548cd61c3a67c536f4a6758
tuananh9201 <tuananh9201@gmail.com>
2022-12-20 10:20:41 +0700

    add onboard page

---
+++ pages/onboard.tsx
@@ -0,0 +1,36 @@
+import { ReactElement } from "react";
+import { NextPageWithLayout } from "./_app";
+import Head from "next/head";
+import OnBoardLayout from "@/components/onboard-layout";
+import Image from "next/image";
+import { backIcon, logo } from "@/constants/images";
+import Link from "next/link";
+const EmptyPage: NextPageWithLayout = () => {
+  return (
+    <>
+      <Head>
+        <title>Onboard</title>
+      </Head>
+      <div>
+        <div className="left"></div>
+        <div className="right">
+          <div className="right-bottom" />
+        </div>
+        <div className="container">
+          <Image src={logo} alt="" />
+          <h3 className="text-intro">Hãy nói cho tôi bạn là?</h3>
+          <button className="ibuild-btn experts">Chuyên gia xây dựng</button>
+          <button className="ibuild-btn users">Người dùng phổ thông</button>
+        </div>
+      </div>
+    </>
+  );
+};
+EmptyPage.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <>
+      <OnBoardLayout>{page}</OnBoardLayout>
+    </>
+  );
+};
+export default EmptyPage;
