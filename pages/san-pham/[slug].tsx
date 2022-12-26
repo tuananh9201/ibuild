c323cf005a57d0ddc0fb1d96446587cbc37bfe51
tuananh9201 <tuananh9201@gmail.com>
2022-12-26 16:47:22 +0700

    fix: css

--- pages/san-pham/[slug].tsx
+++ pages/san-pham/[slug].tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from "react";
 import MainLayout from "@/components/main-layout";
 import { NextPageWithLayout } from "../_app";
-
+import { Row, Col } from "antd";
 import Head from "next/head";
 import style from "@/styles/modules/product-detail.module.scss";
 import suppliers from "../../data/suppliers.json";
@@ -29,78 +29,87 @@ const DetailProduct: NextPageWithLayout = () => {
       </Head>
       <div className="product-main">
         <section className={style.productDetail}>
-          <div className={style.productInfo}>
-            <div className={style.productSlider}></div>
-            <div className={style.productDetailInfo}>
-              <div className={style.productDetailInfoTop}>
-                <h1 className={style.productTitle}>Thiết bị vệ sinh cao cấp</h1>
-                <div className={style.supplier}>
-                  <div className={style.suppLogo}>
-                    <Image src={Supp4} alt="Trung Tâm Vlxd Đồng Tâm" />
+          <Row className={style.productInfo}>
+            <Col md={24} lg={12}>
+              <div className={style.productSlider}></div>
+            </Col>
+            <Col md={24} lg={12}>
+              <div className={style.productDetailInfo}>
+                <div className={style.productDetailInfoTop}>
+                  <h1 className={style.productTitle}>
+                    Thiết bị vệ sinh cao cấp
+                  </h1>
+                  <div className={style.supplier}>
+                    <div className={style.suppLogo}>
+                      <Image src={Supp4} alt="Trung Tâm Vlxd Đồng Tâm" />
+                    </div>
+                    <div className={style.suppName}>
+                      Trung Tâm Vlxd Đồng Tâm
+                    </div>
                   </div>
-                  <div className={style.suppName}>Trung Tâm Vlxd Đồng Tâm</div>
+                  <div className={style.price}>10.000.000 - 30.000.000 VND</div>
+                  <div className={style.infoRow}>
+                    <div className={style.icon}>
+                      <Image src={shoppingBasket} alt="" />
+                    </div>
+                    <div className={style.label}>Số lượng</div>
+                    <div className={style.number}>300 -1000</div>
+                  </div>
+                  <div className={style.infoRow}>
+                    <div className={style.icon}>
+                      <Image src={addressIcon} alt="" />
+                    </div>
+                    <div className={style.label}>
+                      158 Nguyễn Khánh Toàn, Quan Hoa, Cầu Giấy, Hà Nội
+                    </div>
+                  </div>
+                  <div className={style.action}>
+                    <div>
+                      <button
+                        onClick={() => {
+                          // handleClickProduct(supplier.id);
+                        }}
+                        className={style.buttonPhone}
+                      >
+                        <Image src={btnPhoneIconTranf} alt="" />
+                        0272 387 2233
+                      </button>
+                    </div>
+                    <div>
+                      <button className="btn-in-card outline">
+                        <Image src={btnBookmarkIcon} alt="" />
+                        Theo dõi sản phẩm
+                      </button>
+                    </div>
+                    <div>
+                      <button className="btn-in-card outline">
+                        Thông tin nhà cung cấp
+                      </button>
+                    </div>
+                  </div>
                 </div>
-                <div className={style.price}>10.000.000 - 30.000.000 VND</div>
-                <div className={style.infoRow}>
-                  <div className={style.icon}>
-                    <Image src={shoppingBasket} alt="" />
-                  </div>
-                  <div className={style.label}>Số lượng</div>
-                  <div className={style.number}>300 -1000</div>
-                </div>
-                <div className={style.infoRow}>
-                  <div className={style.icon}>
-                    <Image src={addressIcon} alt="" />
-                  </div>
-                  <div className={style.label}>
-                    158 Nguyễn Khánh Toàn, Quan Hoa, Cầu Giấy, Hà Nội
-                  </div>
-                </div>
-                <div className={style.action}>
-                  <div>
-                    <button
-                      onClick={() => {
-                        // handleClickProduct(supplier.id);
-                      }}
-                      className={style.buttonPhone}
-                    >
-                      <Image src={btnPhoneIconTranf} alt="" />
-                      0272 387 2233
-                    </button>
-                  </div>
-                  <div>
-                    <button className="btn-in-card outline">
-                      <Image src={btnBookmarkIcon} alt="" />
-                      Theo dõi sản phẩm
-                    </button>
-                  </div>
-                  <div>
-                    <button className="btn-in-card outline">
-                      Thông tin nhà cung cấp
-                    </button>
-                  </div>
+                <div className={style.productDetailInfoBottom}>
+                  <span>
+                    Thiết bị vệ sinh cao cấp là dòng thiết bị cao cấp bao gồm:{" "}
+                  </span>
+                  <ul>
+                    <li>Bồn cầu vệ sinh</li>
+                    <li>Chậu rửa lavabo</li>
+                    <li>Bồn Tiểu Nam/Nữ</li>
+                    <li>Vòi lavabo cảm ứng</li>
+                    <li>Bộ vòi sen tắm</li>
+                    <li>Van xả tiểu</li>
+                    <li>Vòi rửa chén</li>
+                    <li>Bồn tắm</li>
+                    <li>Vòi xả lạnh</li>
+                    <li>Phụ kiện phòng tắm</li>
+                  </ul>
                 </div>
               </div>
-              <div className={style.productDetailInfoBottom}>
-                <span>
-                  Thiết bị vệ sinh cao cấp là dòng thiết bị cao cấp bao gồm:{" "}
-                </span>
-                <ul>
-                  <li>Bồn cầu vệ sinh</li>
-                  <li>Chậu rửa lavabo</li>
-                  <li>Bồn Tiểu Nam/Nữ</li>
-                  <li>Vòi lavabo cảm ứng</li>
-                  <li>Bộ vòi sen tắm</li>
-                  <li>Van xả tiểu</li>
-                  <li>Vòi rửa chén</li>
-                  <li>Bồn tắm</li>
-                  <li>Vòi xả lạnh</li>
-                  <li>Phụ kiện phòng tắm</li>
-                </ul>
-              </div>
-            </div>
-          </div>
+            </Col>
+          </Row>
           <div className={style.productChartPrice}>
+            <h3 className={style.priceChartTitle}>Lịch sử giá tích lũy</h3>
             <ProductChartPrice />
           </div>
         </section>
