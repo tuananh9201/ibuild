3f3fbc43dcd7b41800f4e60e03f031412c6bbb68
tuananh9201 <tuananh9201@gmail.com>
2023-01-12 13:58:25 +0700

    fix: style supplier page

--- components/supplier/product-groups.tsx
+++ components/supplier/product-groups.tsx
@@ -3,7 +3,11 @@ import ProductCard from "../products/product-card";
 import suppliers from "../../data/suppliers.json";
 const ProductGroups = () => {
   return (
-    <Row gutter={[24, 32]}>
+    <Row
+      gutter={{
+        lg: 32,
+      }}
+    >
       {suppliers.map((supplier) => (
         <Col className="gutter-row" key={supplier.id} md={24} lg={8}>
           <ProductCard supplier={supplier} />
