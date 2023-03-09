6ac22deff750021e0554c055ed6e03c4edf22d65
tuananh9201 <tuananh9201@gmail.com>
2023-03-09 15:03:33 +0700

    remove scripts

--- src/components/onboard-layout.tsx
+++ src/components/onboard-layout.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Script from "next/script";
 
 interface IOnboardLayoutProps {
   children: React.ReactNode;
@@ -12,11 +11,6 @@ export default function OnBoardLayout({ children }: IOnboardLayoutProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <div className="onboard-wrapper">{children}</div>
-      {/* <Script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        crossOrigin="anonymous"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-      /> */}
     </>
   );
 }
