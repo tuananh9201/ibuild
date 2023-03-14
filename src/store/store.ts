4130f3ab0e32fe578c3d760f37241615c67011dc
tuananh9201 <tuananh9201@gmail.com>
2023-03-14 11:04:58 +0700

    add rootReducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,15 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/user/user";
 import authReducer from "./features/auth/auth";
 import registerReducer from "./features/auth/register";
 
+export const rootReducer = combineReducers({
+  user: userReducer,
+  auth: authReducer,
+  register: registerReducer,
+});
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-    auth: authReducer,
-    register: registerReducer,
-  },
+  reducer: rootReducer,
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
