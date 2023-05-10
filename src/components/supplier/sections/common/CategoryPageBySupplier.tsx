641818996fe66c7a71aa815bdbf4b564c6481735
Trang Bui Van <buitrang96bg@gmail.com>
2023-05-10 11:25:29 +0700

    fea pharse4

--- src/components/supplier/sections/common/CategoryPageBySupplier.tsx
+++ src/components/supplier/sections/common/CategoryPageBySupplier.tsx
@@ -3,12 +3,14 @@ import { SearchIcon } from "@/images/icons/product_types/icon_wrapper";
 const CategoryPageBySupplier = () => {
   return (
     <>
-      <div>
-        <SearchIcon />
+      <div className="relative">
+        <div className="absolute left-4 top-1/2 -translate-y-1/2">
+          <SearchIcon />
+        </div>
         <input
           type="text"
           placeholder="Tìm sản phẩm trong nhà cung cấp"
-          className="w-full border border-solid border-[#dddddd] rounded"
+          className="w-full border border-solid border-[#dddddd] rounded py-4 px-12 placeholder:text-text-color placeholder:font-normal placeholder:text-base"
         />
       </div>
     </>
