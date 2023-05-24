ddfda674250edde54dc04852ca02a998a8ff7ac3
Trang Bui Van <buitrang96bg@gmail.com>
2023-05-24 08:56:25 +0700

    fix: title quan-ly-tai-khoan page

--- src/pages/nha-cung-cap/[slug].tsx
+++ src/pages/nha-cung-cap/[slug].tsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { ReactElement, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import useSWR from "swr";
+import Head from "next/head";
 
 import { RenderImageError } from "@/components/common";
 import MainLayout from "@/components/main-layout";
@@ -78,116 +79,124 @@ const NhaCungCap: NextPageWithLayout = () => {
   }, [tab]);
 
   return (
-    <section>
-      <div className="relative">
-        <RenderImageError
-          defaultImage={SupplierBgDefault.src}
-          image={supplier?.cover_image || ""}
-          width={1280}
-          height={230}
-          title="bg supplier"
-          className="mx-auto w-full object-cover"
-        />
-        <div className="w-[170px] h-[170px] rounded-full overflow-hidden absolute bottom-0 left-20 translate-y-1/2">
+    <>
+      <Head>
+        <title>{supplier?.name}</title>
+      </Head>
+      <section>
+        <div className="relative">
           <RenderImageError
-            defaultImage={IBuildLogo.src}
-            image={supplier?.logo || ""}
-            width={170}
-            height={170}
-            title="logo"
-            className="w-[170] h-[170] object-cover"
+            defaultImage={SupplierBgDefault.src}
+            image={supplier?.cover_image || ""}
+            width={1280}
+            height={230}
+            title="bg supplier"
+            className="mx-auto w-full object-cover"
+          />
+          <div className="w-[170px] h-[170px] rounded-full overflow-hidden absolute bottom-0 left-20 translate-y-1/2">
+            <RenderImageError
+              defaultImage={IBuildLogo.src}
+              image={supplier?.logo || ""}
+              width={170}
+              height={170}
+              title="logo"
+              className="w-[170] h-[170] object-cover"
+            />
+          </div>
+        </div>
+        <div className="pt-28 lg:pt-5 lg:pl-[300px]">
+          <div className="flex flex-col gap-4 lg:gap-0 lg:flex-row lg:justify-between items-center">
+            <div>
+              <div className="flex flex-row gap-3 mb-2">
+                {getRangeAddress(supplier?.addresses, false).map((add, idx) => {
+                  if (idx < 2) {
+                    return (
+                      <span
+                        key={idx}
+                        className="flex gap-2 text-text-color font-normal text-base"
+                      >
+                        <LocationIcon className="fill-text-color" />
+                        {add}
+                      </span>
+                    );
+                  } else {
+                    return (
+                      <span
+                        key={idx}
+                        className="flex gap-2 text-text-color font-normal text-base"
+                      >
+                        <LocationIcon className="fill-text-color" />
+                        +3
+                      </span>
+                    );
+                  }
+                })}
+              </div>
+              <h1 className="font-semibold text-[28px] text-text-color leading-[125%] line-clamp-1">
+                {supplier?.name || ""}
+              </h1>
+            </div>
+
+            <button
+              className="flex flex-row items-center gap-3 px-8 py-3 bg-primary-color rounded"
+              onClick={handleClickPhone}
+            >
+              <PhoneIcon className="fill-white" />
+              <span className="text-white text-base font-normal">
+                {!token ? "Đăng nhập để xem thông tin" : supplier?.phone || ""}
+              </span>
+            </button>
+          </div>
+          <div className="grid grid-cols-2 md:grid-cols-3 mt-8 gap-y-3">
+            <div className="flex flex-row gap-2 line-clamp-1">
+              <ShopIcon className="" />
+              <span>Sản phẩm</span>
+              <span>{FormatNumber(supplier?.products || 0)}</span>
+            </div>
+            <div className="flex flex-row gap-2 line-clamp-1">
+              <SupervisionIcon className="" />
+              <span>Người theo dõi</span>
+              <span>{FormatNumber(supplier?.followers || 0)}</span>
+            </div>
+            <div className="flex flex-row gap-2 line-clamp-1">
+              <DateRangeIcon className="" />
+              <span>Ngày tham gia</span>
+              <span>
+                {supplier?.participation_date
+                  ? moment(supplier.participation_date).format("DD/MM/YYYY")
+                  : "Không rõ"}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="mt-14 custom-width-tab">
+          <Tabs
+            activeKey={currentTab}
+            centered
+            items={TABS_NAME}
+            onChange={handleOnchangeTab}
+            tabBarStyle={{
+              width: "500px",
+            }}
           />
         </div>
-      </div>
-      <div className="pt-28 lg:pt-5 lg:pl-[300px]">
-        <div className="flex flex-col gap-4 lg:gap-0 lg:flex-row lg:justify-between items-center">
-          <div>
-            <div className="flex flex-row gap-3 mb-2">
-              {getRangeAddress(supplier?.addresses, false).map((add, idx) => {
-                if (idx < 2) {
-                  return (
-                    <span
-                      key={idx}
-                      className="flex gap-2 text-text-color font-normal text-base"
-                    >
-                      <LocationIcon className="fill-text-color" />
-                      {add}
-                    </span>
-                  );
-                } else {
-                  return (
-                    <span
-                      key={idx}
-                      className="flex gap-2 text-text-color font-normal text-base"
-                    >
-                      <LocationIcon className="fill-text-color" />
-                      +3
-                    </span>
-                  );
-                }
-              })}
-            </div>
-            <h1 className="font-semibold text-[28px] text-text-color leading-[125%] line-clamp-1">
-              {supplier?.name || ""}
-            </h1>
-          </div>
-
-          <button
-            className="flex flex-row items-center gap-3 px-8 py-3 bg-primary-color rounded"
-            onClick={handleClickPhone}
-          >
-            <PhoneIcon className="fill-white" />
-            <span className="text-white text-base font-normal">
-              {!token ? "Đăng nhập để xem thông tin" : supplier?.phone || ""}
-            </span>
-          </button>
-        </div>
-        <div className="grid grid-cols-2 md:grid-cols-3 mt-8 gap-y-3">
-          <div className="flex flex-row gap-2 line-clamp-1">
-            <ShopIcon className="" />
-            <span>Sản phẩm</span>
-            <span>{FormatNumber(supplier?.products || 0)}</span>
-          </div>
-          <div className="flex flex-row gap-2 line-clamp-1">
-            <SupervisionIcon className="" />
-            <span>Người theo dõi</span>
-            <span>{FormatNumber(supplier?.followers || 0)}</span>
-          </div>
-          <div className="flex flex-row gap-2 line-clamp-1">
-            <DateRangeIcon className="" />
-            <span>Ngày tham gia</span>
-            <span>
-              {supplier?.participation_date
-                ? moment(supplier.participation_date).format("DD/MM/YYYY")
-                : "Không rõ"}
-            </span>
-          </div>
-        </div>
-      </div>
-      <div className="mt-14 custom-width-tab">
-        <Tabs
-          activeKey={currentTab}
-          centered
-          items={TABS_NAME}
-          onChange={handleOnchangeTab}
-          tabBarStyle={{
-            width: "500px",
-          }}
-        />
-      </div>
-      {currentTab === "1" && supplier?.id && (
-        <SupplierInformation supplierId={supplier?.id} supplier={supplier} />
-      )}
-      {currentTab === "2" && supplier?.id && (
-        <AllProducts supplierId={supplier.id} categoryId={category as string} />
-      )}
-      {currentTab === "3" && supplier?.id && (
-        <GeneralIntroduction
-          aboutSupplier={supplier.about}
-          supplierId={supplier.id}
-        />
-      )}
-    </section>
+        {currentTab === "1" && supplier?.id && (
+          <SupplierInformation supplierId={supplier?.id} supplier={supplier} />
+        )}
+        {currentTab === "2" && supplier?.id && (
+          <AllProducts
+            supplierId={supplier.id}
+            categoryId={category as string}
+          />
+        )}
+        {currentTab === "3" && supplier?.id && (
+          <GeneralIntroduction
+            aboutSupplier={supplier.about}
+            supplierId={supplier.id}
+          />
+        )}
+      </section>
+    </>
   );
 };
 
